d2ddfa6ed73b3c882ae2d20194773b531e3b8424
qimengchao981 <u1297640@umail.utah.edu>
2025-06-27 19:32:24 -0500

    configure Spring Boot to use MySQL and spring JPA

--- src/main/java/com/example/xiaohongshu_microservices/XiaohongshuMicroservicesApplication.java
+++ src/main/java/com/example/xiaohongshu_microservices/XiaohongshuMicroservicesApplication.java
@@ -2,11 +2,9 @@ package com.example.xiaohongshu_microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.mybatis.spring.annotation.MapperScan;
 
 
 @SpringBootApplication
-@MapperScan("com.example.xiaohongshu_microservices.mapper")
 public class XiaohongshuMicroservicesApplication {
 
 	public static void main(String[] args) {
